262d89456fdc0c147aff3eb765b7f9fe51a25eaa
MinhQuan805 <minhquanwork2006@gmail.com>
2025-09-22 18:27:56 +0700

    Test

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Định nghĩa schema cho body
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"message": "Item received", "data": item}
